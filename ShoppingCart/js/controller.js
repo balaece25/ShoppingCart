f4152a789e2be2a2830c2efdd8c08a17f1a930f6
balaece25 <balaece25@gmail.com>
2014-01-04 22:08:10 +1100

    Initial Changes

---
+++ ShoppingCart/js/controller.js
@@ -0,0 +1,42 @@
+﻿'use strict';
+
+// the storeController contains two objects:
+// - store: contains the product list
+// - cart: the shopping cart object
+function storeController($scope, $routeParams, DataService) {
+
+    // get store and cart from service
+    $scope.store = DataService.store;
+    $scope.cart = DataService.cart;
+
+    // use routing to pick the selected product
+    if ($routeParams.productSku != null) {
+        $scope.product = $scope.store.getProduct($routeParams.productSku);
+    }
+}
+
+
+
+function mycontroller($scope, UserService) {
+    $scope.id = 0;
+    $scope.name = "";
+    $scope.greeting = function () {
+        if (this.name) {
+            return "Hello " + this.name+"!";
+        }
+    };
+    $scope.visible = true;
+    $scope.toggle = function () { this.visible = !this.visible; };
+    $scope.someValue = "";
+    $scope.$watch("someValue", function (newValue, old) {
+        if (newValue !== old) {
+            $scope.changes = "Changed from " + old + " to" + newValue;
+        }
+    });
+    $scope.users = UserService.all();
+    $scope.checked = false;
+}
+
+function AnotherCtrl($scope, UserService) {
+    $scope.firstUser = UserService.first();
+}
---
+++ ShoppingCart/js/directives.js
@@ -0,0 +1,40 @@
+﻿
+//show="visible" directive name = scope property name
+storeApp.directive("show", function () {
+     var myfunction=function (scope, element, attributes) {
+            scope.$watch(attributes.show, function (value) {
+                element.css('display', value ? '' : 'none');
+                element.css('color', 'green');
+            });
+        };
+    return {
+        //scope - scope object
+        //element - defines the element
+        //attributes contains the elements attributes
+        link: myfunction
+
+    };
+});
+
+
+//<myWidget>child paragraph control</mywidget>
+storeApp.directive("myWidget", function () {
+    var linkFunction = function (scope, element, attributes) {
+        var paragraph = element.children()[0];
+         $(paragraph).on("click", function () {
+            $(this).css({ "background-color": "red" });
+        });
+    };
+    return {
+        restrict: "E",
+        link: linkFunction 
+    };
+});
+
+storeApp.directive("myWidget2", function () {
+    return {
+        restrict: "E",
+        template: "<p>Hello World</p>"
+    };
+});
+
